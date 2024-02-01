704af4cd12e922f2e80e557dc312d0bd132ccf5c
Customi24 <maurmaufgfhricioo@gmail.com>
2024-01-31 19:07:20 -0600

    commit 1

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const app = () => {
+  return(
+    <View style={styles.container}> 
+      <Text style={styles.titulo} >Hola mundo</Text>
+      <Text style={styles.parrafo}>Soy Mauricio Morales Gonzalez</Text>
+      <Text style={styles.parrafo2}>Y estoy haciendo mi primer App</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    width: '100%', 
+    height: '100%',
+    backgroundColor: 'black',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: "#FFFFFF",
+    fontFamily: 'Arial',
+  },  
+  parrafo: {
+    fontSize: 25,
+    fontWeight: '300',
+    color: "white",
+  },
+  parrafo2: {
+    fontSize: 25,
+    fontWeight: '300',
+    color: "red",
   },
 });
+
+export default app;
